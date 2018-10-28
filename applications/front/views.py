8180a895eb1d3c1230711e830c526887d6f80839
Anatoly <anatoly@cryptosphere-systems.com>
2018-10-28 18:42:08 +0100

    add a hidden bug

--- applications/front/views.py
+++ applications/front/views.py
@@ -3,3 +3,8 @@ from django.views.generic import TemplateView
 
 class MainView(TemplateView):
     template_name = 'main.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        # hidden bug ^_^
+        # raise ValueError()
+        return super().dispatch(request, *args, **kwargs)
