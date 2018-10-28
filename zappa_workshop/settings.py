d2592ab4efddc18d301eb35c6be9394174a1e4c9
Anatoly <anatoly@cryptosphere-systems.com>
2018-10-28 20:27:35 +0100

    support postgres

--- zappa_workshop/settings.py
+++ zappa_workshop/settings.py
@@ -106,12 +106,20 @@ WSGI_APPLICATION = 'zappa_workshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+SQLITE_DB = {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 }
+POSTGRES_DB = {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': os.getenv('DB_NAME'),
+    'USER': os.getenv('DB_USER'),
+    'PASSWORD': os.getenv('DB_PASSWORD'),
+    'HOST': os.getenv('DB_HOST'),
+    'PORT': os.getenv('DB_PORT'),
+}
+
+DATABASES = SQLITE_DB if DEBUG else POSTGRES_DB
 
 
 # Password validation
